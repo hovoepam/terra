b7230ac33af8262df8fb9c9ea8ee7f4049d176e7
hovoepam <71123090+hovoepam@users.noreply.github.com>
2020-09-19 18:08:36 +0400

    Update main.tf

--- nae/main.tf
+++ nae/main.tf
@@ -1,10 +1,16 @@
 # Configure the Microsoft Azure Provider
-provider "azurerm" { 
+provider "azurerm" {
   features {}
-  }
+  version = "~> 2.0"
 
 # Create a resource group 
-data "azurerm_resource_group" "myterraformgroup" {
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "myResourceGroup"
+    location = "eastus"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
 }
 
 # Create virtual network
@@ -12,7 +18,7 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
     name                = "myVnet"
     address_space       = ["10.0.0.0/16"]
     location            = "eastus"
-    resource_group_name = data.azurerm_resource_group.myterraformgroup.name
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
 
     tags = {
         environment = "Terraform Demo"
